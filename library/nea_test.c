0b85afddb6c205bc12188079cab6cb6f9d85f7c8
Brielle Harrison <nyteshade@gmail.com>
2017-02-12 20:43:38 -0800

    cleaned up directory and built nea_test point isn't working

--- library/nea_test.c
+++ library/nea_test.c
@@ -1,40 +1,73 @@
 ;/* nea_test.c - Execute me to compile me with SAS/C 6.56
-delete nea_test nea_test.(o|info|tmp)
-sc IDIR /src data=far nominc strmer streq nostkchk saveds ign=73 nea_test.c
-slink FROM LIB:c.o,nea_test.o TO nea_test LIB LIB:SC.lib,LIB:Amiga.lib
+delete nea_test nea_test.(o|info|tmp|lnk)
+sc LINK TO nea_test LIB LIB:SC.lib,LIB:Amiga.lib IDIR /src data=far nominc strmer streq nostkchk saveds ign=73 /src/nea/types.c /src/nea/file/strings.c nea_test.c
+delete nea_test.(o|info|tmp|lnk)
 quit
 */
 
 #include <stdio.h>
+#include <string.h>
 
 #include <proto/exec.h>
+#include <proto/dos.h>
 #include <exec/libraries.h>
 
 
 #include <nea/types.h>
+#include <nea/file/strings.h>
 #include <nea/pragmas/nea.h>
 #include <nea/proto/nea.h>
 
 extern struct ExecBase *SysBase;
 
 int main(int argc, char **argv) {
+  BPTR file;
+  NEAFString fstring;
+  NEAString name;
   NEAPoint *p;
   struct Library *NEABase;
   char *result;
 
   NEABase = OpenLibrary("nea.library", 0L);
   if (NEABase) {
-    printf("Library opened\n");
+    Printf("Library opened\n");
 
     result = sayHi(5, "Great");
-    printf("Result %s\n", result);
+    Printf("Result %s\n", result);
 
+    Printf("Creating new integer point at 50,100\n");
     p = NewPointI(50, 100, 0);
-    printf("[x:%ld, y:%ld]\n", p->x.i, p->y.i);
+    Printf("[x:%ld, y:%ld]\n", p->x.i, p->y.i);
+
+    name = (NEAString)AllocVec(sizeof(NEAByte) * 8, MEMF_ANY|MEMF_CLEAR);
+    strcpy(name, "Brielle");
+    SetFString(name, &fstring);
+    Printf("NEAFString [stack]\n");
+    Printf("  value  : %s\n", fstring.value);
+    Printf("  length : %ld\n", fstring.length);
+    Printf("  alloc'd: %s\n\n", fstring.allocated ? "true" : "false");
     
+    Printf("Wrote the above NEAFString to ram:test.bin\n");
+    file = Open("ram:test.bin", MODE_NEWFILE);
+    WriteFString(file, &fstring);
+    Close(file);
+    
+    Printf("Reading the file ram:test.bin\n");
+    file = Open("ram:test.bin", MODE_OLDFILE);
+    ReadFString(file, &fstring);
+    Close(file);
+    
+    Printf("NEAFString [disk]\n");
+    Printf("  value  : %s\n", fstring.value);
+    Printf("  length : %ld\n", fstring.length);
+    Printf("  alloc'd: %s\n\n", fstring.allocated ? "true" : "false");
+    
+    FreeVec(name);
     FreeVec(p);
     FreeVec(result);
     CloseLibrary(NEABase);
+    
+    Printf("Library closed\n");
   }
   else {
     fprintf(stderr, "Failed to open nea.library\n");
